270bf415b8191f86f1a7ee4895083827c482293c
atulya0622 <62475954+atulya0622@users.noreply.github.com>
2020-09-03 22:55:43 +0530

    Update and rename QueueUsingLinkedList.java to QueueTester.java

---
+++ QueueTester.java
@@ -0,0 +1,32 @@
+import java.io.*;
+public class QueueTester {
+
+    public static void main(String[] args)throws IOException{
+        LinkedList list=new LinkedList();
+        int data,n;
+        char ch;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.println("Enter your choice\n1.Enqueue\n2.Dequeue\n3.PRINT");
+        n=Integer.parseInt(br.readLine());
+        switch(n){
+            case 1:{
+                System.out.println("Enter the value\n");
+                data=Integer.parseInt(br.readLine());
+                 list.insertAtTail(data);
+                 System.out.println("Element added successfully\n");
+            }
+            break;
+            case 2:{
+                list.removeFromHead();
+                System.out.println("Element removed successfully\n");
+
+            }
+            break;
+            case 3: list.printList();
+            break;
+        }
+       }
+}
+
+
